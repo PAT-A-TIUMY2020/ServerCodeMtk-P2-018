1ffebdf65d708db2c2137b7de4f6ee5b1cb4c26b
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2020-10-19 20:53:20 +0700

    Add project files.

---
+++ ServerCodeMtk-P2-20180140018/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140018;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace ServerCodeMtk_P2_20180140018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            Uri address = new Uri("http://localhost:8888/Matematika");
+            BasicHttpBinding bind = new BasicHttpBinding();
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika), address);
+                hostObj.AddServiceEndpoint(typeof(IMatematika), bind, "");
+
+                ServiceMetadataBehavior smb = new
+                ServiceMetadataBehavior(); 
+                smb.HttpGetEnabled = true; 
+                hostObj.Description.Behaviors.Add(smb);
+               
+                Binding mexbind =
+               MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObj.AddServiceEndpoint(typeof(IMetadataExchange),
+               mexbind, "mex");
+                hostObj.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
